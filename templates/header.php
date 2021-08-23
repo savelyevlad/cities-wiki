c2b354088dcc33c73e50840fc300f6cf9b1ffdf8
savelyevlad <savelyevlad@gmail.com>
2021-08-23 22:12:37 +0200

    + header TODO: - sidebar + main

---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-light bg-light">
+	<span class="navbar-text" style="margin-left: 2%;">
+		<h1 style="float:left;">Wikipedia</h1>
+	</span>
+	<div style="float:right; margin-right: 3%; margin-top:1%; margin-bottom:1%;">
+		<button type="button" class="btn btn-secondary" style="height:40px;" onclick="onDonateClick()">
+			<p>Donate us</p>
+		</button>
+	</div>
+</nav>
---
+++ templates/sidebar.php
@@ -0,0 +1,41 @@
+<div class="flex-shrink-0 p-3 bg-white" style="width: 15%; position:absolute;">
+			<ul class="list-unstyled ps-0">
+				<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+						Home
+					</button>
+					<div class="collapse" id="home-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="#" class="link-dark rounded">Overview</a></li>
+							<li><a href="#" class="link-dark rounded">Updates</a></li>
+							<li><a href="#" class="link-dark rounded">Reports</a></li>
+						</ul>
+					</div>
+				</li>
+				<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+					Dashboard
+					</button>
+					<div class="collapse" id="dashboard-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="#" class="link-dark rounded">Overview</a></li>
+							<li><a href="#" class="link-dark rounded">Updates</a></li>
+							<li><a href="#" class="link-dark rounded">Reports</a></li>
+						</ul>
+					</div>
+				</li>
+				<li class="border-top my-3"></li>
+				<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+					Orders
+					</button>
+					<div class="collapse" id="orders-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="#" class="link-dark rounded">Overview</a></li>
+							<li><a href="#" class="link-dark rounded">Updates</a></li>
+							<li><a href="#" class="link-dark rounded">Reports</a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
